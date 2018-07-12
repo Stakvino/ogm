1a165026595dbb90927a0a6f71286bbbb2784f6f
Stakvino <stakvino@gmail.com>
2018-07-12 23:59:23 +0100

    create new map

--- js/app.js
+++ js/app.js
@@ -1,21 +1,15 @@
-const tabSelectors = {
-  block    : document.querySelector("div.tab-selectors"),
-  children : document.querySelectorAll("div.tab-selector")
-}
-const editBlocks = {
-  block    : document.querySelector("div.edit-blocks"),
-  children : document.querySelectorAll("div.edit-block")
-}
+const tabSelectors = document.querySelector("div.tab-selectors");
+const editBlocks   = document.querySelector("div.edit-blocks");
 
-tabSelectors.children.forEach(function(tabSelector){
-  //attach event handler to every tab selectors
+//switching tabs event handler
+tabSelectors.querySelectorAll("div.tab-selector").forEach(function(tabSelector){
+
   tabSelector.addEventListener("click",function(){
-
     //Remove selected style from the current selected tab
-    tabSelectors.block.querySelector("div.selected-tab").classList.remove("selected-tab");
+    tabSelectors.querySelector("div.selected-tab").classList.remove("selected-tab");
 
     //hide the current selected block
-    const currentEditBlock = editBlocks.block.querySelector(".show");
+    const currentEditBlock = editBlocks.querySelector(".show");
     currentEditBlock.classList.remove("show");
     currentEditBlock.classList.add("hide");
 
@@ -25,9 +19,28 @@ tabSelectors.children.forEach(function(tabSelector){
     //get the selected edit block using the name of his tab
     const className = this.textContent + "-block";
     //show the selected edit block
-    const selectedEditBlock = editBlocks.block.querySelector(`div.${className}`);
+    const selectedEditBlock = editBlocks.querySelector(`div.${className}`);
     selectedEditBlock.classList.remove("hide");
     selectedEditBlock.classList.add("show");
-
   });
+
+});
+
+//remove default dotted outline when button is focused with js to avoid firefox bug
+document.querySelectorAll("button").forEach( (button) => {
+  button.addEventListener("mousedown",function(){
+    this.style.outline = "none";
+  });
+});
+
+//clicking on create new map button will show the edit map block
+document.querySelector(`button[name="create map"]`).addEventListener("click",() => {
+    document.querySelector("div.map-edit").classList.remove("hide");
+    document.querySelector("div.map-list").classList.add("hide");
+});
+
+//clicking on the map list button will get you back to the map list block
+document.querySelector(`button[name="map list"]`).addEventListener("click",() => {
+    document.querySelector("div.map-list").classList.remove("hide");
+    document.querySelector("div.map-edit").classList.add("hide");
 });
